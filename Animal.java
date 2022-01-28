943426c11a978285e37e020a42ed2e37f1c42790
larap <larapineda2003@gmail.com>
2022-01-28 14:59:38 -0600

    Actualización 2

--- Animal.java
+++ Animal.java
@@ -2,21 +2,21 @@ package Semana_2;
 
 public class Animal {
     // Atributos
-    String nombreCientifico;
-    String nombreComun;
-    String habitat;
-    String alimentacion;
-    String rasgos;
-    int vida;
+    private static String nombreCientifico;
+    private static String nombreComun;
+    private static String habitad;
+    private static String alimentacion;
+    private static String rasgos;
+    private static int vida;
 
     // Constructor
-    public void Animal(String nombreCientifico, String nombreComun, String habitat, String alimentacion, String rasgos, int vida) {
-        this.nombreCientifico = nombreCientifico;
-        this.nombreComun = nombreComun;
-        this.habitat = habitat;
-        this.alimentacion = alimentacion;
-        this.rasgos = rasgos;
-        this.vida = vida;
+    public Animal(String nombreCientifico, String nombreComun, String habitad, String alimentacion, String rasgos, int vida) {
+        Animal.nombreCientifico = nombreCientifico;
+        Animal.nombreComun = nombreComun;
+        Animal.habitad = habitad;
+        Animal.alimentacion = alimentacion;
+        Animal.rasgos = rasgos;
+        Animal.vida = vida;
     }
 
     // Métodos
@@ -24,52 +24,52 @@ public class Animal {
         return nombreCientifico;
     }
 
-    public void setNombreCientifico(String nombreCientifico) {
-        this.nombreCientifico = nombreCientifico;
+    public static void setNombreCientifico(String nombreCientifico) {
+        Animal.nombreCientifico = nombreCientifico;
     }
 
     public String getNombreComun() {
         return nombreComun;
     }
 
-    public void setNombreComun(String nombreComun) {
-        this.nombreComun = nombreComun;
+    public static void setNombreComun(String nombreComun) {
+        Animal.nombreComun = nombreComun;
     }
 
-    public String getHabitat() {
-        return habitat;
+    public String getHabitad() {
+        return habitad;
     }
 
-    public void setHabitat(String habitat) {
-        this.habitat = habitat;
+    public static void setHabitad(String habitad) {
+        Animal.habitad = habitad;
     }
 
     public String getAlimentacion() {
         return alimentacion;
     }
 
-    public void setAlimentacion(String alimentacion) {
-        this.alimentacion = alimentacion;
+    public static void setAlimentacion(String alimentacion) {
+        Animal.alimentacion = alimentacion;
     }
 
     public String getRasgos() {
         return rasgos;
     }
 
-    public void setRasgos(String rasgos) {
-        this.rasgos = rasgos;
+    public static void setRasgos(String rasgos) {
+        Animal.rasgos = rasgos;
     }
 
     public int getVida() {
         return vida;
     }
 
-    public void setVida(int vida) {
-        this.vida = vida;
+    public static void setVida(int vida) {
+        Animal.vida = vida;
     }
 
     public String toString() {
-        return "Nombre científico: " + nombreCientifico + "\nNombre común: " + nombreComun + "\nHabitat: " + habitat +
+        return "Nombre científico: " + nombreCientifico + "\nNombre común: " + nombreComun + "\nHabitad: " + habitad +
                 "\nAlimentación: " + alimentacion + "\nRasgos: " + rasgos + "\nVida: " + vida;
     }
 }
--- Animales.java
+++ Animales.java
@@ -1,8 +1,10 @@
 package Semana_2;
 
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Animales {
+    public static Scanner leer = new Scanner(System.in);
 
     // Atributos
     private static boolean verify = false;
@@ -17,14 +19,83 @@ public class Animales {
         }
     }
 
-    public static void registerAnimal(Animal animal) {
+    public static void registerAnimal(String nombreCientifico) {
         if (!verify) {
-            listaAnimales.add(animal);
+            System.out.println("Ingrese el nombre común del animal:");
+            String nombreComun = leer.nextLine();
+            System.out.println("Ingrese el habitad del animal:");
+            String habitad = leer.nextLine();
+            System.out.println("Ingrese la alimentación del animal:");
+            String alimentacion = leer.nextLine();
+            System.out.println("Ingrese los rasgos del animal:");
+            String rasgos = leer.nextLine();
+            System.out.println("Ingrese la vida del animal:");
+            int vida = leer.nextInt();
+            Animal animal = new Animal(nombreCientifico,nombreComun, habitad, alimentacion, rasgos, vida);
         } else {
             System.out.println("El animal ya existe");
         }
     }
 
-
-
+    public static void editAnimal(String nombreCientifico){
+        for (int i = 0; i < listaAnimales.size(); i++) {
+            if (listaAnimales.get(i).getNombreCientifico().equals(nombreCientifico)) {
+                System.out.println("""
+                        Desea editar todos los datos o solo uno?
+                        1. Editar todos los datos
+                        2. Editar solo un dato
+                        Seleccione una opción:""");
+                int opcion = leer.nextInt();
+                if (opcion == 1) {
+                    System.out.println("Ingrese el nuevo nombre científico:");
+                    Animal.setNombreCientifico(leer.next());
+                    System.out.println("Ingrese el nuevo nombre común:");
+                    Animal.setNombreComun(leer.next());
+                    System.out.println("Ingrese la nueva habitad:");
+                    Animal.setHabitad(leer.next());
+                    System.out.println("Ingrese el nuevo tipo de alimentación:");
+                    Animal.setAlimentacion(leer.next());
+                    System.out.println("Ingrese los nuevos tipos de rasgos:");
+                    Animal.setRasgos(leer.next());
+                    System.out.println("Ingrese la nueva cantidad de vida:");
+                    Animal.setVida(leer.nextInt());
+                    System.out.println("Se han editado todos los datos del animal!");
+                } else if (opcion == 2) {
+                    System.out.println("""
+                            Seleccione el dato que desea editar:
+                            1. Nombre científico
+                            2. Nombre común
+                            3. Habitad
+                            4. Alimentación
+                            5. Rasgos
+                            6. Vida
+                            Seleccione una opción:""");
+                    int opcion2 = leer.nextInt();
+                    if (opcion2 == 1) {
+                        System.out.println("Ingrese el nuevo nombre científico:");
+                        Animal.setNombreCientifico(leer.next());
+                    } else if (opcion2 == 2) {
+                        System.out.println("Ingrese el nuevo nombre común:");
+                        Animal.setNombreComun(leer.next());
+                    } else if (opcion2 == 3) {
+                        System.out.println("Ingrese la nueva habitad:");
+                        Animal.setHabitad(leer.next());
+                    } else if (opcion2 == 4) {
+                        System.out.println("Ingrese el nuevo tipo de alimentación:");
+                        Animal.setAlimentacion(leer.next());
+                    } else if (opcion2 == 5) {
+                        System.out.println("Ingrese los nuevos tipos de rasgos:");
+                        Animal.setRasgos(leer.next());
+                    } else if (opcion2 == 6) {
+                        System.out.println("Ingrese la nueva cantidad de vida:");
+                        Animal.setVida(leer.nextInt());
+                    } else {
+                        System.out.println("Opción inválida");
+                    }
+                } else {
+                    System.out.println("Opción inválida");
+                }
+            }
+        }
+    }
 }
